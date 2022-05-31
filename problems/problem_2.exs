9d5889436fac428fc68e3420566699b25621c12a
zahrah-kargo <zahrah.febriani@kargo.tech>
2022-05-31 14:59:38 +0700

    refine again problem 2

--- problems/problem_2.exs
+++ problems/problem_2.exs
@@ -3,28 +3,32 @@ items = IO.gets("list: ")
 items = String.replace(items, "\n", "") |> String.split(" ", trim: true)
 IO.inspect(items)
 
+defmodule Item do
+  def filterItems(val, acc) do
+    case Integer.parse(val) do
+      {int_val, ""} when Bitwise.band(int_val,1) == 1 ->
+        Map.merge(acc, %{
+          number: acc.number ++ [val],
+          odd_number: acc.odd_number ++ [val]
+        })
+
+      {int_val, ""} when Bitwise.band(int_val,1) == 0 ->
+        Map.merge(acc, %{
+          number: acc.number ++ [val],
+          even_number: acc.even_number ++ [val]
+        })
+
+      _ -> Map.put(acc, :text, acc.text ++ [val])
+    end
+  end
+end
+
 result = Enum.reduce(items, %{
   number: [],
   odd_number: [],
   even_number: [],
   text: []
-}, fn val, acc ->
-  case Integer.parse(val) do
-    {int_val, ""} when Bitwise.band(int_val,1) == 1 ->
-      Map.merge(acc, %{
-        number: acc.number ++ [val],
-        odd_number: acc.odd_number ++ [val]
-      })
-
-    {int_val, ""} when Bitwise.band(int_val,1) == 0 ->
-      Map.merge(acc, %{
-        number: acc.number ++ [val],
-        even_number: acc.even_number ++ [val]
-      })
-
-    _ -> Map.put(acc, :text, acc.text ++ [val])
-  end
-end)
+}, fn val, acc -> Item.filterItems(val, acc) end)
 
 IO.inspect result
 
