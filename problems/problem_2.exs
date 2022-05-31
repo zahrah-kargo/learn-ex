9a3fc329f7116e894310b15804baddc5ff6fb19d
zahrah-kargo <zahrah.febriani@kargo.tech>
2022-05-31 14:53:33 +0700

    refine problem 2

--- problems/problem_2.exs
+++ problems/problem_2.exs
@@ -10,13 +10,13 @@ result = Enum.reduce(items, %{
   text: []
 }, fn val, acc ->
   case Integer.parse(val) do
-    {int_val, ""} when rem(int_val, 2) == 1 ->
+    {int_val, ""} when Bitwise.band(int_val,1) == 1 ->
       Map.merge(acc, %{
         number: acc.number ++ [val],
         odd_number: acc.odd_number ++ [val]
       })
 
-    {int_val, ""} when rem(int_val, 2) == 0 ->
+    {int_val, ""} when Bitwise.band(int_val,1) == 0 ->
       Map.merge(acc, %{
         number: acc.number ++ [val],
         even_number: acc.even_number ++ [val]
